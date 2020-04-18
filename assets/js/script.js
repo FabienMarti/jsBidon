7f0488c032fe3e844ce15096ed75096769055fc1
FabienMarti <martiretti.fabien@gmail.com>
2020-04-18 13:17:05 +0200

    retirer possiblité de zoom user, ajoute opacity bouton valider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 var array = [];
+document.getElementById('buttonValid').style.opacity = 0;
 function getValue(val) {
     array.push(val);
     document.getElementById('text').innerHTML += val + " ";
     if (array.length <= 1) {
         document.getElementById('textError').innerHTML = "Ton choix :";
-
+    }
+    else if (array.length == 3) {
+        document.getElementById('buttonValid').style.opacity = 1;
     }
     else if (array.length >= 4) {
         document.getElementById('textError').innerHTML = "3 Elements MAX";
         document.getElementById('text').innerHTML = " ";
+        document.getElementById('buttonValid').style.opacity = 0;
         array = [];
     }
 }
@@ -27,6 +31,7 @@ function checkResult() {
 
     }
     array = [];
+    document.getElementById('buttonValid').style.opacity = 0;
 }
 
 
