5823c99393f9156874d8f708f355cf6df58d148b
FabienMarti <martiretti.fabien@gmail.com>
2020-04-18 12:59:54 +0200

    nieme ajout

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,19 @@ var array = [];
 function getValue(val) {
     array.push(val);
     document.getElementById('text').innerHTML += val + " ";
-    if (array.length >= 4) {
+    if (array.length <= 1) {
+        document.getElementById('textError').innerHTML = "Ton choix :";
+
+    }
+    else if (array.length >= 4) {
         document.getElementById('textError').innerHTML = "3 Elements MAX";
         document.getElementById('text').innerHTML = " ";
         array = [];
     }
 }
+
+
+
 function checkResult() {
     if (array[0] == 2 && array[1] == 2 && array[2] == 1) {
         document.getElementById('textError').innerHTML = "C'est gagné !";
